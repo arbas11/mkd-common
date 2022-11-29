27cd61556ac88bc11c8333b55b32f728506a1cf0
Ganasa18 <ganasa18@yahoo.com>
2022-11-29 14:14:49 +0700

    Update GlobalHandler Kafka

--- src/utils/controllerError.ts
+++ src/utils/controllerError.ts
@@ -1,18 +1,42 @@
 import { Request, Response, NextFunction } from "express";
 
 export const globalErrorHandler = (
-  err: any,
+  err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   // check status code
-  err.statusCode = err.statusCode || 500;
+  const statusCode = 500;
   // check error message
-  err.status = err.status || "error";
+  const status = "error";
+
+  let message = err.message;
+
+  if (err.name === "KafkaJSNumberOfRetriesExceeded") {
+    message = "Error From Broker Connection";
+    process.exit(1);
+  }
   //   respone error
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
+  res.status(statusCode).json({
+    status: status,
+    message: message,
   });
 };
+
+// export const globalErrorHandler = (
+//   err: any,
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   // check status code
+//   err.statusCode = err.statusCode || 500;
+//   // check error message
+//   err.status = err.status || "error";
+//   //   respone error
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message,
+//   });
+// };
